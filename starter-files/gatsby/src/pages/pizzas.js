bf1eab01ff97662c6f79f8201539d9d9b038ace9
anamwp <anam@themexpert.com>
2020-12-07 23:52:59 +0600

    load sample data in sanity and view it inside pizzalist component

--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import PizzaList from '../components/PizzaList';
 
-export default function PizzaPage(){
+export default function PizzaPage({data}){
+    const pizzas = data.pizzas.nodes;
     return <>
         <h2>This is Pizza page.</h2>
+        <PizzaList pizzas={pizzas}/>
     </>
 }
+
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes{
+                name
+                id
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+                image {
+                    asset {
+                        fluid(maxWidth: 400){
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
