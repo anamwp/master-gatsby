5a6fd317a3e88e6c2f146ece74e89320e820d576
anamwp <anam@themexpert.com>
2020-12-10 07:14:34 +0600

    dynamically create pizza pages

---
+++ starter-files/gatsby/src/templates/Pizza.js
@@ -0,0 +1,26 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+
+export default function singlePizza(){
+    return <p>Single Pizza</p>
+}
+export const query = graphql`
+    query($slug: String!){
+        pizza: sanityPizza(slug: { current: {eq: $slug} }){
+            name
+            id
+            image {
+                asset{
+                    fluid(maxWidth:800){
+                        ...GatsbySanityImageFluid
+                    }
+                }
+            }
+            toppings {
+                name
+                id
+                vegetarian
+            }
+        }
+    }
+`;
