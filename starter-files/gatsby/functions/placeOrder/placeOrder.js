5ea30d94134b36e710db04c308fe432fe512098e
anamwp <anam@themexpert.com>
2020-12-30 02:59:55 +0600

    41 complete - coding serverless function

--- starter-files/gatsby/functions/placeOrder/placeOrder.js
+++ starter-files/gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,24 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({order, total}){
+    return `<div>
+        <h2>Your recent order for ${total}</h2>
+        <p>Please start walking over, we will have your order ready in the next 20 mins.</p>
+        <ul>
+            ${order.map( item => `<li>
+                <img src="${item.thumbnail}" alt="${item.name}"/>
+                ${item.size} ${item.name} - ${item.price}
+            </li>` ).join('')}
+        </ul>
+        <p>Your total is <strong>${total}</strong> due at pickup</p>
+        <style>
+            ul{
+                list-style:none;
+            }
+        </style>
+    </div>`
+}
+
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: 587,
@@ -10,15 +29,29 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+    const body = JSON.parse(event.body);
+    console.log(body);
+    const requiredFields = ['email', 'name', 'order'];
+    for( const field of requiredFields){
+        console.log(`checking that ${field} is good`);
+        if(!body[field]){
+            return {
+                statusCode: 400, 
+                body: JSON.stringify({
+                    message: `Oops! you are missing the ${field} field`,
+                })
+            }
+        }
+    }
+
     const info = await transporter.sendMail({
         from: "Slick's slices <slick@example.com>",
-        to: 'orders@example.com',
+        to: `${body.name} ${body.email}, orders@example.com`,
         subject: 'New Order!',
-        html: `<p>You new order is here!</p>`,
+        html: generateOrderEmail({order: body.order, total: body.total}),
     });
-    console.log('info', info);
     return {
         statusCode: 200,
-        body: JSON.stringify(info)
+        body: JSON.stringify({message: 'Success'})
     }
 }
