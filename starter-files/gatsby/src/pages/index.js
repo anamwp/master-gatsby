68ffde8342d3f9e6357ecdac173be8c952e8c30f
anamwp <anam@themexpert.com>
2021-01-01 03:29:18 +0600

    custom hook for client side data fetching

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import useLatestData from '../utils/useLatestData'
+
+function CurrentlySlicing(){
+    return (
+        <div>
+            <p>Currently Slicing</p>
+        </div>
+    )
+}
+
+function HotSlices(){
+    return (
+        <div>
+            <p>HotSlices</p>
+        </div>
+    )
+}
+
 export default function HomePage(){
-    return <>
-        <h2>Hello world</h2>
-    </>
+    const {slicemasters, hotSlices} = useLatestData();
+    return (
+        <div className="center">
+            <h1>The best pizza Downtown!</h1>
+            <p>Open 11am to 11pm Every Single Day</p>
+            <div>
+                <CurrentlySlicing slicemasters={slicemasters} />
+                <HotSlices HotSlices={hotSlices} />
+            </div>
+        </div>
+    )
 }
